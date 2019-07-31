7de8cf15e597ee5b1fe98ca8c12aad82266d75c4
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2019-07-31 12:38:31 +0300

    Simplify the lexer interface

--- src/LexerInterface.php
+++ src/LexerInterface.php
@@ -15,9 +15,6 @@ use Phplrt\Contracts\Lexer\Exception\RuntimeExceptionInterface;
 
 /**
  * An interface that is an abstract implementation of a lexer.
- *
- * An implementation should be a lexical analyser, i.e. split a string into
- * a set of lexeme (tokens).
  */
 interface LexerInterface
 {
@@ -28,30 +25,28 @@ interface LexerInterface
      *
      * <code>
      *  $source = new Readable('<?php if (false) { return true; }');
-     *  $lexer  = new PhpLexer();
      *
-     *  foreach ($lexer->lex($source) as $token) {
-     *      echo vsprintf('%5s | %10s | "%s"' . "\n", [
+     *  foreach ((new PhpLexer())->lex($source) as $token) {
+     *      echo vsprintf('%5s | "%s"' . "\n", [
      *          $token->getType(),
-     *          $lexer->nameOf($token->getType()),
      *          $token->getValue()
      *      ]);
      *  }
      *
-     *  >> 379  | T_OPEN_TAG    | "<?php "
-     *  >> 327  | T_IF          | "if"
-     *  >> 382  | T_WHITESPACE  | " "
-     *  >> 0    | UNKNOWN       | "("
-     *  >> 319  | T_STRING      | "false"
-     *  >> 0    | UNKNOWN       | ")"
-     *  >> 382  | T_WHITESPACE  | " "
-     *  >> 0    | UNKNOWN       | "{"
-     *  >> 348  | T_RETURN      | "return"
-     *  >> 382  | T_WHITESPACE  | " "
-     *  >> 319  | T_STRING      | "true"
-     *  >> 0    | UNKNOWN       | ";"
-     *  >> 382  | T_WHITESPACE  | " "
-     *  >> 0    | UNKNOWN       | "}"
+     *  >> 379 | "<?php "
+     *  >> 327 | "if"
+     *  >> 382 | " "
+     *  >> 0   | "("
+     *  >> 319 | "false"
+     *  >> 0   | ")"
+     *  >> 382 | " "
+     *  >> 0   | "{"
+     *  >> 348 | "return"
+     *  >> 382 | " "
+     *  >> 319 | "true"
+     *  >> 0   | ";"
+     *  >> 382 | " "
+     *  >> 0   | "}"
      * </code>
      *
      * @param ReadableInterface $source
@@ -61,12 +56,4 @@ interface LexerInterface
      * @throws RuntimeExceptionInterface
      */
     public function lex(ReadableInterface $source): iterable;
-
-    /**
-     * Get the symbolic name of a given token id.
-     *
-     * @param int $id
-     * @return string
-     */
-    public function nameOf(int $id): string;
 }
