a7e2a038d2f74fef706fa35a87485af02a44c7ff
SerafimArts <nesk@xakep.ru>
2024-02-04 18:42:42 +0000

    Add contracts tests

---
+++ tests/CompatibilityTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phplrt\Contracts\Lexer\Tests;
+
+use Phplrt\Contracts\Lexer\LexerInterface;
+use Phplrt\Contracts\Lexer\TokenInterface;
+use PHPUnit\Framework\Attributes\Group;
+
+#[Group('phplrt/lexer-contracts')]
+class CompatibilityTest extends TestCase
+{
+    public function testLexerCompatibility(): void
+    {
+        self::expectNotToPerformAssertions();
+
+        new class implements LexerInterface {
+            public function lex($source): iterable {}
+        };
+    }
+
+    public function testTokenCompatibility(): void
+    {
+        self::expectNotToPerformAssertions();
+
+        new class implements TokenInterface {
+            public function getName(): string {}
+            public function getOffset(): int {}
+            public function getValue(): string {}
+            public function getBytes(): int {}
+        };
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phplrt\Contracts\Lexer\Tests;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+#[Group('phplrt/lexer-contracts')]
+abstract class TestCase extends BaseTestCase {}
