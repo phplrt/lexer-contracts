933456cd9c2c546901eb6497189dcf9d53372d05
SerafimArts <nesk@xakep.ru>
2024-07-28 13:23:38 +0000

    Add PHP 8.4 tests support

--- tests/CompatibilityTest.php
+++ tests/CompatibilityTest.php
@@ -40,8 +40,11 @@ class CompatibilityTest extends TestCase
 
         new class () implements TokenInterface {
             public function getName(): string {}
+
             public function getOffset(): int {}
+
             public function getValue(): string {}
+
             public function getBytes(): int {}
         };
     }
